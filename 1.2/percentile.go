9a3f101641772330eb0dde0fbd82b98c3bf474e1
Quin Quintero <quinq@gravitational.com>
2020-09-09 09:57:30 -0700

    change generatePercentile return

--- 1.2/percentile.go
+++ 1.2/percentile.go
@@ -3,25 +3,22 @@ package percentile
 import (
 	"errors"
 	"math"
+	"sort"
 )
 
-type percentile func([]uint) (uint, error)
+type percentile func([]uint) uint
 
-func generatePercentile(x float32) percentile {
+func generatePercentile(x float32) (percentile, error) {
+	if x > 1.0 || x < 0.0 {
+		return nil, errors.New("input out of range")
+	}
 
-	f := func(data []uint) (uint, error) {
-		if x > 1.0 || x < 0.0 {
-			return 0, errors.New("input out of range")
-		}
-		if x == 1.0 {
-			return data[len(data)-1], nil
-		} else if x == 0.0 {
-			return data[0], nil
-		}
+	f := func(data []uint) uint {
+		sort.Slice(data, func(i, j int) bool { return data[i] < data[j] })
 		length := len(data)
 		n := x * float32(length)
 		index := uint(math.Ceil(float64(n)))
-		return data[index-1], nil
+		return data[index-1]
 	}
-	return f
+	return f, nil
 }
--- 1.2/percentile_test.go
+++ 1.2/percentile_test.go
@@ -6,53 +6,58 @@ import (
 )
 
 func TestGeneratePercentile(t *testing.T) {
+
 	var tests = []struct {
-		fn percentile
-		p  uint
+		input float32
+		p     uint
 	}{
-		{generatePercentile(.05), 15},
-		{generatePercentile(.3), 20},
-		{generatePercentile(.4), 20},
-		{generatePercentile(.5), 35},
-		{generatePercentile(1.0), 50},
+		{.05, 0},
+		{.3, 15},
+		{.4, 20},
+		{.5, 20},
+		{1.0, 50},
 	}
-	data := []uint{15, 20, 35, 40, 50}
+	data := []uint{15, 40, 35, 20, 50, 0}
+
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("PASS"), func(t *testing.T) {
-			if output, err := test.fn(data); output != test.p || err != nil {
+			if output, err := generatePercentile(test.input); err != nil {
+				t.Errorf("Got error, exprected no error")
+			} else if output(data) != test.p {
 				t.Errorf("Fail expected: %v", test.p)
-			}
 
+			}
 		})
 	}
 }
 
 func TestGeneratePercentileSecond(t *testing.T) {
 	var tests = []struct {
-		fn percentile
-		p  uint
+		input float32
+		p     uint
 	}{
-		{generatePercentile(.25), 7},
-		{generatePercentile(.5), 8},
-		{generatePercentile(.75), 15},
-		{generatePercentile(1.0), 20},
+		{.25, 7},
+		{.5, 8},
+		{.75, 15},
+		{1.0, 20},
 	}
-	data := []uint{3, 6, 7, 8, 8, 10, 13, 15, 16, 20}
+	data := []uint{16, 6, 7, 8, 8, 10, 13, 15, 3, 20}
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("PASS"), func(t *testing.T) {
-			if output, _ := test.fn(data); output != test.p {
+			if output, err := generatePercentile(test.input); err != nil {
+				t.Errorf("Got error, exprected no error")
+			} else if output(data) != test.p {
 				t.Errorf("Fail expected: %v", test.p)
+
 			}
 		})
 	}
 }
 
 func TestGenerateInvalidInput(t *testing.T) {
-	var fn percentile
-	fn = generatePercentile(1.1)
-	data := []uint{3, 9, 10, 13, 15, 16, 20}
+	_, err := generatePercentile(1.1)
 
-	if _, err := fn(data); err == nil {
+	if err == nil {
 		t.Error("Expected error")
 
 	}
