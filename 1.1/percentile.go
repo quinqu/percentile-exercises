c9f3d0cddac479c5582fe9dba4ee163207a96ee7
Quin Quintero <quinq@gravitational.com>
2020-09-08 17:47:42 -0700

    sorted slice

--- 1.1/percentile.go
+++ 1.1/percentile.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sort"
 )
 
 // createData creates unordered data spanning the range 0-n
@@ -17,6 +18,12 @@ func createData(rand *rand.Rand, n uint) []uint {
 }
 
 type percentile func([]uint) uint
+type seedData []uint
+func (a seedData) Len() int           { return len(a) }
+
+func (a seedData) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+func (a seedData) Less(i, j int) bool { return a[i] < a[j] }
 
 func main() {
 	var seed int64 = 1
@@ -28,7 +35,9 @@ func main() {
 		index := uint(math.Ceil(float64(n)))
 		return data[index]
 	}
-	data := createData(rand, 10000)
+	data := seedData(createData(rand, 10000))
+	sort.Sort(data)
 	p90 := f(data)
+	fmt.Print(data)
 	fmt.Println(p90)
 }
